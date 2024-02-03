20e7b7085a18f1037d3946fd685b035fbb590467
MoeFingers <mbzuiter@gmail.com>
2024-02-03 13:49:56 -0700

    added readme and places placeholder

---
+++ README.md
@@ -0,0 +1,3 @@
+# Project REST-Rant
+
+REST-Rant is a web application where users can post, edit, and delete reviews.
--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 require("dotenv").config()
-const express = require("express")
-const app = express()
+
+
+const app = require("express")()
+// const app = express()
+
+
 
 //const path = require("path")
 
-
 //app.use(express.static('public'))
-
-app.get("/", (req,res) => {
-    res.send("hello restaurant world")
-})
 
 // app.get("*", (req,res) => {
 //     res.sendFile(path.join(__dirname, 'public/nord404.tsparticles/nord/404.html'))
 // })
 
+
+app.use("/places", require("./controllers/places"))
+
+app.get("/", (req,res) => {
+    res.send("hello restaurant world")
+})
+
 app.get("*", (req,res) => {
     res.status(404).send(`the most definitely happy and certainly not unhappy 404`)
 })
-
-
 
 app.listen(process.env.PORT, () => {
     console.log(`server is running on ${process.env.PORT}`)
