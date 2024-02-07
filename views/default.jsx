88f70cbcc939452b38ef59763355df9c83fb4e58
MoeFingers <mbzuiter@gmail.com>
2024-02-06 21:06:20 -0700

    added to default view for layout

--- views/default.jsx
+++ views/default.jsx
@@ -1,2 +1,13 @@
-const React = require('react')
-
+function Def (html) {
+    return (
+        <html>
+            <head>
+                <title>Title</title>
+            </head>
+            <body>
+                {html.children}
+            </body>
+        </html>
+    )
+  }
+  
