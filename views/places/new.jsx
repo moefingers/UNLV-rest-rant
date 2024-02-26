fc244e793414d2648904cef7143682f282b33c1b
MoeFingers <mbzuiter@gmail.com>
2024-02-25 19:35:37 -0700

    minor fixes

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -29,7 +29,7 @@ function new_form () {
                 </div>
                 <div className="form-group">
                     <label htmlFor="founded">Founded Year</label>
-                    <input className="form-control" id="founded" name="founded" />
+                    <input className="form-control" type="number" id="founded" name="founded" defaultValue={new Date().getFullYear()}/>
                 </div>
 
                 <input className='btn btn-primary' type="submit" value="Add Place" />
